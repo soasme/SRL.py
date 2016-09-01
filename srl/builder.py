c8e61992e1916f3c0d3d152a547cf6d6935ca305
soasme <soasme@gmail.com>
2016-09-01 14:14:52 +0800

    test email simple format.

--- srl/builder.py
+++ srl/builder.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import re
 import copy
 
 class Builder(object):
 
     def __init__(self):
         self.regex = []
+        self.flags = 0
+        self.compiled = None
 
     def literally(self, char):
         if char in {'+', '\\'}:
@@ -14,10 +17,14 @@ class Builder(object):
         return self
 
     def digit(self):
-        self.regex.append(r'\d')
+        self.regex.append(r'[0-9]')
         return self
 
     number = digit
+
+    def letter(self):
+        self.regex.append(r'[a-z]')
+        return self
 
     def between(self, start, end):
         self.regex.append(r'{%d,%d}' % (start, end))
@@ -27,10 +34,46 @@ class Builder(object):
         self.regex.append(r'+')
         return self
 
+    def atLeast(self, number):
+        self.regex.append(r'{%d,}?' % number)
+        return self
+
+    def anyOf(self, conditions):
+        builder = Builder()
+        subquery = conditions(builder)
+        regex = subquery.get(r'|')
+        self.regex.append('(?:%s)' % regex)
+        return self
+
+    eitherOf = anyOf
+
+    def oneOf(self, chars):
+        self.regex.append(r'[%s]' % chars)
+        return self
+
     def mustEnd(self):
         self.regex.append(r'$')
         return self
 
-    def get(self):
-        return r''.join(self.regex)
+    def caseInsensitive(self):
+        self.flags = self.flags | re.IGNORECASE
+        return self
 
+    def get(self, implode=r''):
+        return implode.join(self.regex)
+
+    def compile(self):
+        self.compiled = re.compile(self.get(), self.flags)
+        return self
+
+    def is_valid(self):
+        try:
+            self.compile()
+            return True
+        except re.error:
+            return False
+
+    def is_matching(self, string):
+        if not self.compiled:
+            self.compile()
+        return self.compiled.match(string, self.flags)
--- tests/test_builder.py
+++ tests/test_builder.py
@@ -14,3 +14,23 @@ def test_simple_phone_number_format():
     assert not re.match(regex, '49 123-45')
     assert not re.match(regex, 'a+49 123-45')
     assert not re.match(regex, '+49 123-45b')
+
+def test_simple_email_format():
+    reg = Builder().anyOf(
+        lambda q: q.digit().letter().oneOf('._%+-')
+    ).onceOrMore().literally('@').eitherOf(
+        lambda q: q.digit().letter().oneOf('.-')
+    ).onceOrMore().literally('.').letter().atLeast(2) \
+    .mustEnd().caseInsensitive()
+    regex = reg.get()
+
+    assert reg.is_valid()
+    assert reg.is_matching('super-He4vy.add+ress@top-Le.ve1.domains')
+    assert not reg.is_matching('sample.example.com')
+    assert re.match(regex, 'sample@example.com')
+    assert re.match(regex, 'super-He4vy.add+ress@top-Le.ve1.domains', reg.flags)
+    assert not re.match(regex, 'sample.example.com')
+    assert not re.match(regex, 'missing@tld')
+    assert not re.match(regex, 'hav ing@spac.es')
+    assert not re.match(regex, 'no@pe.123')
+    assert not re.match(regex, 'invalid@email.com123')
