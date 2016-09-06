fff75b875291d85d8956b8ea223faf1c148d3476
soasme <soasme@gmail.com>
2016-09-06 16:51:33 +0800

    define tokens.

---
+++ srl/parsers/main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import lex
+import yacc
+
+# List of token names.
+
+tokens = (
+    'SINGLE_QUOTE',
+    'DOUBLE_QUOTE',
+    'WHITESPACE',
+    'LEFT_PARENTHESIS',
+    'RIGHT_PARENTHESIS'
+    'NUMBER',
+    'COMMA',
+)
+
+
